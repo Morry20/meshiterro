b687abfc0b8dab6f5b95d92b988f410c024a8026
Morry20 <ku.l.taf54@gmail.com>
2021-06-22 06:37:37 +0000

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b9a64baff209f1ccad07bbf2f2ecf47a42384219d43ad873a709498fff06ddd1263cd8235766ac123efcb82b82328387395307781128604c6d99496708d64efb'
